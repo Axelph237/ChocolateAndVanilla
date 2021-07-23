54356547995449f552f0be30972cbcc7d97003ce
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2021-07-22 21:45:22 -0700

    New Stuff

--- src/main/java/net/candy/chocolatevanilla/Main.java
+++ src/main/java/net/candy/chocolatevanilla/Main.java
@@ -17,7 +17,7 @@ public class Main implements ModInitializer {
 	public static final Item GLOW_BERRY_PIE = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(5F/6F).build()));
 	public static final Item CARROT_CAKE = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(5F/6F).build()));
 	public static final Item BEETROOT_SEED_CAKE = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(10).saturationModifier(13F/10F).build()));
-	public static final Item BEEF_STROGANOFF = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(12).saturationModifier(12F/14F).build()));
+	public static final MushroomStewItem BEEF_STROGANOFF = new MushroomStewItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(12).saturationModifier(12F/14F).build()));
 	public static final Item JAM_SANDWICH = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).build()));
 	public static final Item HONEY_SANDWICH = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).build()));
 	public static final Item CHOCOLATE_MILK = new MilkBucketItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.5F).build()));
@@ -27,8 +27,8 @@ public class Main implements ModInitializer {
 	public static final Item BRAISED_BAMBOO = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(5).saturationModifier(1F).build()));
 	public static final Item ROASTED_PUMPKIN_SEEDS = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(3).saturationModifier(2F/3F).snack().build()));
 	public static final Item ROASTED_MELON_SEEDS = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(3).saturationModifier(2F/3F).snack().build()));
-	public static final Item SQUID_INK_PASTA = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(8).saturationModifier(6F/8F).build()));
-	public static final Item GLOW_SQUID_INK_PASTA = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(5).saturationModifier(4F/5F).statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 200, 0), 1F).build()));
+	public static final MushroomStewItem SQUID_INK_PASTA = new MushroomStewItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(8).saturationModifier(6F/8F).build()));
+	public static final MushroomStewItem GLOW_SQUID_INK_PASTA = new MushroomStewItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(5).saturationModifier(4F/5F).statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 200, 0), 1F).build()));
 	public static final Item CHICKEN_SANDWICH = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(9F/8F).build()));
 	public static final Item FISH_SANDWICH = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(7).saturationModifier(8F/7F).build()));
 	public static final Item SUSHI = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(9).saturationModifier(1F).build()));
@@ -38,11 +38,11 @@ public class Main implements ModInitializer {
 	public static final TeaItem ROSE_TEA = new TeaItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.5F).build()));
 	public static final TeaItem WITHER_ROSE_TEA = new TeaItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.5F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 1F).build()));
 	public static final Item CANDIED_TULIP_PETALS = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(3).saturationModifier(1F/3F).snack().build()));
-	public static final Item PUFFERFISH_STEW = new MushroomStewItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(9).saturationModifier(13F/9F).build()));
+	public static final MushroomStewItem PUFFERFISH_STEW = new MushroomStewItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(9).saturationModifier(13F/9F).build()));
 	public static final Item FRIED_EGG = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(4).saturationModifier(4F).build()));
-	public static final Item SCALDING_STEW = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(14).saturationModifier(1F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 1F).build()));
-	public static final TeaItem SPIKED_BERRY_TEA = new TeaItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 0), 1F).build()));
-	public static final TeaItem DRAGONS_SPIRIT = new TeaItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 1F).build()));
+	public static final MushroomStewItem SCALDING_STEW = new MushroomStewItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(14).saturationModifier(1F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 1F).build()));
+	public static final TeaItem SPIKED_BERRY_TEA = new TeaItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 0), 1F).build()));
+	public static final TeaItem DRAGONS_SPIRIT = new TeaItem(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 1F).build()));
 	public static final Item CHICHARONES = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(7F/8F).snack().build()));
 	public static final Item JELLIED_ENDER_EYE = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(7).saturationModifier(6F/7F).snack().build()));
 
